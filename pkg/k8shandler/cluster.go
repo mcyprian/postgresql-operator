251a34df002732e533db506fe0391328a6dd380a
Michal Cyprian <m.cyprian@gmail.com>
2019-06-29 10:25:44 +0200

    Add primary service

--- pkg/k8shandler/cluster.go
+++ pkg/k8shandler/cluster.go
@@ -7,6 +7,14 @@ import (
 )
 
 var nodes map[string]Node
+var primaryNode *Node
+
+func getOne(m map[string]postgresqlv1.PostgreSQLNode) (string, *postgresqlv1.PostgreSQLNode, error) {
+	for name, node := range m {
+		return name, &node, nil
+	}
+	return "", nil, fmt.Errorf("Empty map, cannot get a key")
+}
 
 // CreateOrUpdateCluster iterates over all nodes in the current spec
 // and ensures cluster reflect desired state
@@ -16,6 +24,18 @@ func CreateOrUpdateCluster(request *PostgreSQLRequest) (bool, error) {
 
 	if nodes == nil {
 		nodes = make(map[string]Node)
+	}
+	if primaryNode == nil {
+		// Create new primary node TODO: lost primary reference?
+		name, specNode, err := getOne(request.cluster.Spec.Nodes)
+		if err != nil {
+			return false, fmt.Errorf("Nodes spec is empty, cannot choose master node")
+		}
+		node := newDeploymentNode(request, name, specNode, true)
+		if err := node.create(request); err != nil {
+			return false, err
+		}
+		nodes[name] = node
 	}
 	for name, specNode := range request.cluster.Spec.Nodes {
 		node, ok := nodes[name]
@@ -27,7 +47,7 @@ func CreateOrUpdateCluster(request *PostgreSQLRequest) (bool, error) {
 			}
 		} else {
 			// Create a new node and add it into nodes map
-			node = newDeploymentNode(request, name, &specNode)
+			node = newDeploymentNode(request, name, &specNode, false)
 			if err := node.create(request); err != nil {
 				return false, err
 			}
