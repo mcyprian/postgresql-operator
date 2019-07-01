4f999a12871bc8d9d99e0777772c88056584c37a
Michal Cyprian <m.cyprian@gmail.com>
2019-07-01 21:40:59 +0200

    Refactor error handling in sql, add role to status

--- pkg/k8shandler/sql.go
+++ pkg/k8shandler/sql.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	_ "github.com/lib/pq"
+	postgresqlv1 "github.com/mcyprian/postgresql-operator/pkg/apis/postgresql/v1"
 )
 
 type databaseInfo struct {
@@ -18,8 +19,9 @@ type databaseInfo struct {
 }
 
 type database struct {
-	info   databaseInfo
-	engine *sql.DB
+	info      databaseInfo
+	engine    *sql.DB
+	cachedErr error
 }
 
 func newRepmgrDatabase(host string) *database {
@@ -31,6 +33,7 @@ func newRepmgrDatabase(host string) *database {
 			dbname:  "repmgr",
 			sslmode: "disable",
 		},
+		cachedErr: nil,
 	}
 }
 
@@ -39,62 +42,112 @@ func (info *databaseInfo) connectionString() string {
 		info.host, info.port, info.user, info.dbname, info.sslmode)
 }
 
-func (db *database) initialize() error {
-	var err error
-	db.engine, err = sql.Open("postgres", db.info.connectionString())
-	if err != nil {
-		return err
+func (db *database) err() error {
+	err := db.cachedErr
+	db.cachedErr = nil
+	return err
+}
+
+func (db *database) initialize() {
+	if db.cachedErr == nil {
+		db.engine, db.cachedErr = sql.Open("postgres", db.info.connectionString())
 	}
-	return nil
 }
 
 func (db *database) ping() error {
+	if db.cachedErr != nil {
+		return db.cachedErr
+	}
+
 	return db.engine.Ping()
 }
 
-func (db *database) version() (string, error) {
+// version returns version of PostgreSQL server
+func (db *database) version() string {
 	var version string
+
+	if db.cachedErr != nil {
+		return "unknown"
+	}
+
 	row := db.engine.QueryRow("SELECT version()")
-	if err := row.Scan(&version); err != nil {
-		return "", err
+	if db.cachedErr = row.Scan(&version); db.cachedErr != nil {
+		return "unknown"
 	}
 	fields := strings.Fields(version)
 	if len(fields) <= 2 {
-		return "", fmt.Errorf("Failed to retrieve PostgreSQL version")
+		db.cachedErr = fmt.Errorf("Failed to retrieve PostgreSQL version")
+		return "unknown"
 	}
-	return fields[1], nil
+	return fields[1]
 }
 
 // repmgrNodesExists checks whether repmgr.nodes table was created already
-func (db *database) repmgrNodesExists() (bool, error) {
+func (db *database) repmgrNodesExists() bool {
 	var result bool
-	row := db.engine.QueryRow("SELECT EXISTS ( SELECT 1 FROM information_schema.tables WHERE table_schema = 'repmgr' AND table_name = 'nodes')")
-	if err := row.Scan(&result); err != nil {
-		return false, err
+
+	if db.cachedErr != nil {
+		return false
 	}
-	return result, nil
+
+	row := db.engine.QueryRow("SELECT EXISTS ( SELECT 1 FROM information_schema.tables WHERE table_schema = 'repmgr' AND table_name = 'nodes')")
+	if db.cachedErr = row.Scan(&result); db.cachedErr != nil {
+		return false
+	}
+	return result
 }
 
 // isRegistered checks whether node name is present in repmgr.nodes table
-func (db *database) isRegistered(nodeName string) (bool, error) {
+func (db *database) isRegistered(nodeName string) bool {
 	var result int
+	var stmt *sql.Stmt
+
+	if db.cachedErr != nil {
+		return false
+	}
 
 	// if repmgr.nodes table is missing, node is not registered
-	exists, err := db.repmgrNodesExists()
-	if err != nil {
-		return false, err
-	}
-	if !exists {
-		return false, nil
+	exists := db.repmgrNodesExists()
+	if db.cachedErr != nil || !exists {
+		return false
 	}
 
 	// repmgr.nodes table exists, check if it contains row for the node
-	query, err := db.engine.Prepare("SELECT COUNT(*) FROM repmgr.nodes WHERE node_name = $1")
-	if err != nil {
-		return false, err
+	stmt, db.cachedErr = db.engine.Prepare("SELECT COUNT(*) FROM repmgr.nodes WHERE node_name = $1")
+	if db.cachedErr != nil {
+		return false
 	}
-	if err := query.QueryRow(nodeName).Scan(&result); err != nil {
-		return false, err
+	if db.cachedErr = stmt.QueryRow(nodeName).Scan(&result); db.cachedErr != nil {
+		return false
 	}
-	return result == 1, nil
+	return result == 1
+}
+
+// getRole retrieves current node role inside repmgr cluster
+func (db *database) getRole(nodeName string) postgresqlv1.PostgreSQLNodeRole {
+	var result string
+	var stmt *sql.Stmt
+
+	if db.cachedErr != nil {
+		return postgresqlv1.PostgreSQLNodeRoleUnknown
+	}
+
+	// if repmgr.nodes table is missing, role is unknown
+	exists := db.repmgrNodesExists()
+	if db.cachedErr != nil || !exists {
+		return postgresqlv1.PostgreSQLNodeRoleUnknown
+	}
+
+	// repmgr.nodes table exists, check the role
+	stmt, db.cachedErr = db.engine.Prepare("SELECT type FROM repmgr.nodes WHERE node_name = $1")
+	if db.cachedErr != nil {
+		return postgresqlv1.PostgreSQLNodeRoleUnknown
+	}
+	if db.cachedErr = stmt.QueryRow(nodeName).Scan(&result); db.cachedErr != nil {
+		return postgresqlv1.PostgreSQLNodeRoleUnknown
+	}
+	if result == postgresqlv1.PostgreSQLNodeRolePrimary {
+		return postgresqlv1.PostgreSQLNodeRolePrimary
+	}
+	return postgresqlv1.PostgreSQLNodeRoleStandby
 }
