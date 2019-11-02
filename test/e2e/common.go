1b35ed8d694a5e6b8df64918fdf96d806d91eb13
Michal Cyprian <m.cyprian@gmail.com>
2019-11-02 15:31:25 +0100

    test: add automated failover test
    Divide e2e test into smaller files for better readibility.

---
+++ test/e2e/common.go
@@ -0,0 +1,91 @@
+package e2e
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	goctx "context"
+	postgresqlv1 "github.com/mcyprian/postgresql-operator/pkg/apis/postgresql/v1"
+
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type getStatusFunc func(f *framework.Framework, namespace string) error
+
+func retryExecution(t *testing.T, f *framework.Framework, namespace string, fce getStatusFunc, retries int, timeout time.Duration) error {
+	attempt := -1
+	for {
+		attempt++
+		t.Log(fmt.Sprintf("getStatus execution, attempt number %v", attempt))
+		if attempt > 0 {
+			time.Sleep(timeout)
+		}
+		if err := fce(f, namespace); err != nil {
+			if attempt >= retries {
+				return err
+			}
+		} else {
+			return nil
+		}
+	}
+}
+
+func getStatusDouble(f *framework.Framework, namespace string) error {
+	exampleName := types.NamespacedName{Name: postgreSQLCRName, Namespace: namespace}
+	current := &postgresqlv1.PostgreSQL{}
+	if err := f.Client.Get(goctx.TODO(), exampleName, current); err != nil {
+		return fmt.Errorf("Failed to get examplePostgreSQL: %v", err)
+	}
+	for name, status := range current.Status.Nodes {
+		if name == "primary-node" {
+			if status.Role != "primary" {
+				return fmt.Errorf("Wrong node role or status, expected (%v, %v), got: (%v, %v)", "primary-node", "primary", name, status.Role)
+			}
+		} else {
+			if status.Role != "standby" {
+				return fmt.Errorf("Wrong node role or status, expected (%v, %v), got: (%v, %v)", name, "standby", name, status.Role)
+			}
+		}
+	}
+	return nil
+}
+
+func getStatusSingle(f *framework.Framework, namespace string) error {
+	exampleName := types.NamespacedName{Name: postgreSQLCRName, Namespace: namespace}
+	current := &postgresqlv1.PostgreSQL{}
+
+	if err := f.Client.Get(goctx.TODO(), exampleName, current); err != nil {
+		return fmt.Errorf("Failed to get examplePostgreSQL: %v", err)
+	}
+	for name, status := range current.Status.Nodes {
+		if name == "primary-node" {
+			if status.Role != "primary" || status.Priority != 100 {
+				return fmt.Errorf("Wrong node role or status, expected (%v, %v), got: (%v, %v)", "primary", 100, status.Role, status.Priority)
+			}
+		} else {
+			return fmt.Errorf("Wrong node name %v, only primary-node should be present in the cluster", name)
+		}
+	}
+	return nil
+}
+
+func getStatusFailover(f *framework.Framework, namespace string) error {
+	exampleName := types.NamespacedName{Name: postgreSQLCRName, Namespace: namespace}
+	current := &postgresqlv1.PostgreSQL{}
+
+	if err := f.Client.Get(goctx.TODO(), exampleName, current); err != nil {
+		return fmt.Errorf("Failed to get examplePostgreSQL: %v", err)
+	}
+	for name, status := range current.Status.Nodes {
+		if name == "standby-node" {
+			if status.Role != "primary" || status.Priority != 80 {
+				return fmt.Errorf("Wrong node role or status, expected (%v, %v), got: (%v, %v)", "primary", 80, status.Role, status.Priority)
+			}
+		} else {
+			return fmt.Errorf("Wrong node name %v, only standby-node should be present in the cluster", name)
+		}
+	}
+	return nil
+}
---
+++ test/e2e/failover.go
@@ -0,0 +1,137 @@
+package e2e
+
+import (
+	goctx "context"
+	"fmt"
+	"testing"
+	"time"
+
+	postgresqlv1 "github.com/mcyprian/postgresql-operator/pkg/apis/postgresql/v1"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func PostgreSQLClusterFailover(t *testing.T) {
+	t.Parallel()
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
+	if err := ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval}); err != nil {
+		t.Fatalf("Failed to initialize cluster resources: %v", err)
+	}
+	t.Log("Cluster resources initialized")
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Namespace found: %v", namespace)
+	// get global framework variables reference
+	f := framework.Global
+	// wait for operator to be ready
+	if err := e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "postgresql-operator", 1, retryInterval, timeout); err != nil {
+		t.Fatal(err)
+	}
+	if err = PostgreSQLClusterFailoverTest(t, f, ctx); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func PostgreSQLClusterFailoverTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		return fmt.Errorf("Couldn't get namespace: %v", err)
+	}
+	exampleName := types.NamespacedName{Name: postgreSQLCRName, Namespace: namespace}
+	cpuValue, _ := resource.ParseQuantity("100m")
+	memValue, _ := resource.ParseQuantity("250M")
+	resources := corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			corev1.ResourceCPU:    cpuValue,
+			corev1.ResourceMemory: memValue,
+		},
+		Requests: corev1.ResourceList{
+			corev1.ResourceCPU:    cpuValue,
+			corev1.ResourceMemory: memValue,
+		},
+	}
+
+	primaryNode := postgresqlv1.PostgreSQLNode{
+		Priority:  100,
+		Resources: resources,
+		Storage:   postgresqlv1.PostgreSQLStorageSpec{},
+	}
+	standbyNode := postgresqlv1.PostgreSQLNode{
+		Priority:  80,
+		Resources: resources,
+		Storage:   postgresqlv1.PostgreSQLStorageSpec{},
+	}
+
+	examplePostgreSQL := &postgresqlv1.PostgreSQL{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "PostgreSQL",
+			APIVersion: postgresqlv1.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      postgreSQLCRName,
+			Namespace: namespace,
+		},
+		Spec: postgresqlv1.PostgreSQLSpec{
+			ManagementState: postgresqlv1.ManagementStateManaged,
+			Nodes: map[string]postgresqlv1.PostgreSQLNode{
+				"primary-node": primaryNode,
+				"standby-node": standbyNode,
+			},
+		},
+		Status: postgresqlv1.PostgreSQLStatus{
+			Nodes: map[string]postgresqlv1.PostgreSQLNodeStatus{
+				"primary-node": postgresqlv1.PostgreSQLNodeStatus{},
+				"standby-node": postgresqlv1.PostgreSQLNodeStatus{},
+			},
+		},
+	}
+
+	if err := f.Client.Create(goctx.TODO(), examplePostgreSQL, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval}); err != nil {
+		return fmt.Errorf("Failed to create example PostgreSQL: %v", err)
+	}
+	if err := e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "primary-node", 1, retryInterval, timeout); err != nil {
+		return fmt.Errorf("Waiting for deployment primary-node timed out: %v", err)
+	}
+	if err := e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "standby-node", 1, retryInterval, timeout); err != nil {
+		return fmt.Errorf("Waiting for deployment standby-node timed out: %v", err)
+	}
+	if err := retryExecution(t, f, namespace, getStatusDouble, 7, time.Second*10); err != nil {
+		return err
+	}
+	t.Log("Initial deployment created.")
+	primaryDeployment, err := f.KubeClient.AppsV1().Deployments(namespace).Get("primary-node", metav1.GetOptions{IncludeUninitialized: true})
+	if err != nil {
+		return fmt.Errorf("Failed to get primary-node deployment: %v", err)
+	}
+	current := &postgresqlv1.PostgreSQL{}
+	if err := f.Client.Get(goctx.TODO(), exampleName, current); err != nil {
+		return fmt.Errorf("Failed to get examplePostgreSQL: %v", err)
+	}
+	delete(current.Spec.Nodes, "primary-node")
+
+	if err := f.Client.Update(goctx.TODO(), current); err != nil {
+		return fmt.Errorf("Failed to update cluster: %v", err)
+	}
+	if err := e2eutil.WaitForDeletion(t, f.Client.Client, primaryDeployment, retryInterval, timeout); err != nil {
+		return fmt.Errorf("Waiting for primary-node deletion timed out: %v", err)
+	}
+	t.Log("Primary node downscale success.")
+
+	if err := e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "standby-node", 1, retryInterval, timeout); err != nil {
+		return fmt.Errorf("Waiting for deployment standby-node timed out: %v", err)
+	}
+	if err = retryExecution(t, f, namespace, getStatusFailover, 7, time.Second*10); err != nil {
+		return err
+	}
+	t.Log("Failover success.")
+
+	t.Log("Success")
+	return nil
+}
